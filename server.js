e73e9d49a93a23fe187776646c34aecd6491eacb
MelynaPernia <mebipeos@outlook.com>
2018-02-16 07:04:48 -0500

    Añadiendo mas informacion

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const server = app.listen(3000, listening);
+const server = app.listen(8080, listening);
 function listening(){
     console.log('Encendido')
 }
